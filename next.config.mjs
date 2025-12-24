f11aeae3d71961a5fdb346aaedb0363e3785aaad
Muhammad Ghayas <muhammadghayas352@gmail.com>
2025-12-24 15:35:05 +0500

    Configure Next.js for Cloudflare Pages static export

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: "export",
   images: {
-    formats: ["image/avif", "image/webp"],
-    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-    minimumCacheTTL: 31536000
+    unoptimized: true,
+    formats: ["image/avif", "image/webp"]
   },
   compress: true,
   poweredByHeader: false
